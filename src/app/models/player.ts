d702248df68263515d242a52f248a792c64b803d
Berkin Sansal <berkin.sansal@siemens.com>
2023-11-23 04:40:31 +0300

    player model updated

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -1,13 +1,20 @@
 import { Gender } from './enums/gender.enum';
 
 export class Player {
+    overall = 0; // 0-10
 
     constructor(
         public name: string,
         public gender: Gender,
-        public serving: number,
-        public hitting: number,
-        public blocking: number,
-        public defense: number,
-    ) { }
+        public serving: number, // 0-10
+        public hitting: number, // 0-10
+        public blocking: number, // 0-10
+        public defense: number, // 0-10
+    ) {
+        this.calculateOverall();
+    }
+
+    calculateOverall() {
+        this.overall = (this.serving + this.hitting + this.blocking + this.defense) / 4;
+    }
 }
