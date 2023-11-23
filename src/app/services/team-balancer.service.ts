c7b243c80d487047383183d6eef493cc95a92429
Berkin Sansal <berkin.sansal@siemens.com>
2023-11-23 04:40:42 +0300

    service updated

--- src/app/services/team-balancer.service.ts
+++ src/app/services/team-balancer.service.ts
@@ -5,39 +5,40 @@ import { Player } from '../models/player';
     providedIn: 'root'
 })
 export class TeamBalancerService {
+    players: Player[] = [];
+    team1: Player[] = [];
+    team2: Player[] = [];
 
     constructor() { }
 
-    balanceTeams(players: Player[]): [Player[], Player[]] {
+    balanceTeams() {
         // Sort players based on their skill levels
-        players.sort((a, b) => this.calculateSkillLevel(b) - this.calculateSkillLevel(a));
+        this.players.sort((a, b) => this.calculateSkillLevel(b) - this.calculateSkillLevel(a));
 
         // Split into two teams
-        const team1: Player[] = [];
-        const team2: Player[] = [];
+        this.team1 = [];
+        this.team2 = [];
 
-        for (let i = 0; i < players.length / 2; i++) {
+        for (let i = 0; i < this.players.length / 2; i++) {
             if (i % 2 === 0) {
                 // one round put better player to team 1
-                team1.push(players[i * 2]);
-                if (i + 1 < players.length) {
-                    team2.push(players[i * 2 + 1]);
+                this.team1.push(this.players[i * 2]);
+                if (i + 1 < this.players.length) {
+                    this.team2.push(this.players[i * 2 + 1]);
                 }
             } else {
                 // other round put better player to team 2
-                team2.push(players[i * 2]);
-                if (i + 1 < players.length) {
-                    team1.push(players[i * 2 + 1]);
+                this.team2.push(this.players[i * 2]);
+                if (i + 1 < this.players.length) {
+                    this.team1.push(this.players[i * 2 + 1]);
                 }
             }
         }
-
-        return [team1, team2];
     }
 
     private calculateSkillLevel(player: Player): number {
         // Implement the logic to calculate the player's overall skill level
         // For example, a simple average of the skills:
-        return (player.serving + player.hitting + player.blocking + player.defense) / 4;
+        return player.overall;
     }
 }
